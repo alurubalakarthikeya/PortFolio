197df12b43a47185e76ed3feb0348df3fbeec70c
Karthikeya Aluru <codingprofession@gmail.com>
2024-08-21 10:30:28 +0530

    animations

--- script.js
+++ script.js
@@ -96,5 +96,62 @@ function updateIconPosition() {
   icon.style.top = `${newPosition}px`;
 }
 
+/*Scroll Animation*/
+document.addEventListener("DOMContentLoaded", function() {
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.fade-in-up-target').forEach(el => {
+  gsap.fromTo(el, 
+    { opacity: 0, filter: blur(5), y: 100 },
+    {
+      opacity: 1,
+      filter: blur(0),
+      y: 0,
+      duration: 2,
+      scrollTrigger: {
+        trigger: el,
+        start: 'top 95%',
+        end: 'bottom 10%',
+        scrub: true
+      }
+    }
+  );
+});
+
+gsap.utils.toArray('.fade-in-left-target').forEach(el => {
+  gsap.fromTo(el, 
+    { opacity: 0, x: -100 }, // Start state
+    {
+      opacity: 1,
+      x: 0, // End state
+      duration: 2,
+      scrollTrigger: {
+        trigger: el,
+        start: 'top 98%',
+        end: 'bottom 50%',
+        scrub: true
+      }
+    }
+  );
+});
+
+gsap.utils.toArray('.fade-in-right-target').forEach(el => {
+  gsap.fromTo(el, 
+    { opacity: 0}, 
+    {
+      opacity: 1,
+      duration: 2,
+      scrollTrigger: {
+        trigger: el,
+        start: 'top 95%',
+        end: 'bottom 10%',
+        scrub: true
+      }
+    }
+  );
+});
+});
+/*Scroll Animation*/
+
 
 
